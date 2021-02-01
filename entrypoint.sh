c6b4e7350a75ae49bd04fe2a687fff6acd0b08a4
ismferd <fernandez.molina.ismael@gmail.com>
2021-02-01 18:15:43 +0100

    debugging

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,4 +12,5 @@ export KUBECONFIG=${KUBECONFIG}/.kube/config
 
 
 POD_NAME=$(kubectl get pods -n ${NAMESPACE} -l app=${APP} | grep -v NAME | cut -d' ' -f1 | head -n 1)
+echo ${POD_NAME}
 /usr/local/bin/krsync -av --progress --stats ${SOURCE_DIR} ${POD_NAME}@${NAMESPACE}:${DEST_DIR}

669bd65302f248c0578741a93ed274dcdf853f60
ismferd <fernandez.molina.ismael@gmail.com>
2021-02-01 15:37:43 +0100

    modifyng

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,4 +8,4 @@ SOURCE_DIR=$3
 DEST_DIR=$4
 
 POD_NAME=$(kubectl get pods -n ${NAMESPACE} -l app=${APP} | grep -v NAME | cut -d' ' -f1 | head -n 1)
-./krsync -av --progress --stats ${SOURCE_DIR} ${POD_NAME}@${NAMESPACE}:${DEST_DIR}
+krsync -av --progress --stats ${SOURCE_DIR} ${POD_NAME}@${NAMESPACE}:${DEST_DIR}

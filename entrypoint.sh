ad492e6df376535eadca400cc8b34708c1d2868c
ismferd <fernandez.molina.ismael@gmail.com>
2021-02-01 13:59:47 +0100

    action firt try

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,10 @@
 #!/bin/sh -l
 
-echo "Hello $1"
-time=$(date)
-echo ::set-output name=time::$time
+set -eu
+
+NAMESPACE=$1
+APP=$2
+DIR=$3
+
+POD_NAME=$(kubectl get pods -n ${NAMESPACE} -l app=${APP} | grep -v NAME | cut -d' ' -f1 | head -n 1)
+./krsync -av --progress --stats .git/* ${POD_NAME}@${NAMESPACE}:${DIR}

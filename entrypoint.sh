331138e5ae81cb75d3b069e5ba393f1c2482c930
ismferd <fernandez.molina.ismael@gmail.com>
2021-02-02 09:19:38 +0100

    final step

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,6 +11,4 @@ DEST_DIR=$5
 aws eks update-kubeconfig --name ${CLUSTER} --region ${AWS_DEFAULT_REGION}
 POD_NAME=$(kubectl get pods -n ${NAMESPACE} -l app=${APP} | grep -v NAME | cut -d' ' -f1 | head -n 1)
 
-ls -la /usr/local/bin/krsync
-ls -la /usr/local/bin/kubectl
 krsync -av --progress --stats ${SOURCE_DIR} ${POD_NAME}@${NAMESPACE}:${DEST_DIR}

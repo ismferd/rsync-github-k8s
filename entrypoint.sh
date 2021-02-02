bcbf03cae1d798634954184919142f8780187e58
ismferd <fernandez.molina.ismael@gmail.com>
2021-02-02 08:34:03 +0100

    debugging

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,16 +2,18 @@
 
 set -eu
 
-NAMESPACE=$1
-APP=$2
-SOURCE_DIR=$3
-DEST_DIR=$4
-KUBECONFIG=$5
+CLUSTER=$1
+NAMESPACE=$2
+APP=$3
+SOURCE_DIR=$4
+DEST_DIR=$5
+KUBECONFIG=$6
 
 export KUBECONFIG=${KUBECONFIG}/.kube/config
 env
+aws eks update-kubeconfig --name ${CLUSTER} --region ${AWS_DEFAULT_REGION}
 POD_NAME=$(kubectl get pods -n ${NAMESPACE} -l app=${APP} | grep -v NAME | cut -d' ' -f1 | head -n 1)
-echo ${POD_NAME}
+
 ls -la /usr/local/bin/krsync
 ls -la /usr/local/bin/kubectl
 krsync -av --progress --stats ${SOURCE_DIR} ${POD_NAME}@${NAMESPACE}:${DEST_DIR}
